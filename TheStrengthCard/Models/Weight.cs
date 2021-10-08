154f6f834e973e9b56b9873991415d7a1d4cf622
Nick Yelle <yellencicholas@gmail.com>
2021-10-08 11:17:00 -0400

    adding models

---
+++ TheStrengthCard/Models/Weight.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    class Weight
+    {
+        //Properties.
+        public int Id { get; set; }
+        public DateTime DateTime { get; set; }
+        public int Amount { get; set; }
+        public int ClientId { get; set; }
+        public int ExercieId { get; set; }
+
+        //Constructor.
+        public Weight() { }
+    }
+}
